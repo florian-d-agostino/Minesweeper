6baf9e59ed8d8981320419423d7287b0fef41255
Hykoo13 <haikmonossiandev@gmail.com>
2026-03-24 14:30:57 +0100

    correct init and add some init and methods

--- src/backend/case.py
+++ src/backend/case.py
@@ -1,7 +1,21 @@
 class Case : 
-    __init__(self):
+    def __init__(self):
         self.value = 0
         self.is_bomb = False
+        self.is_revealed = False
+        self.is_marked = 0
 
     def transform_case(self):
         self.is_bomb = True
+
+    def reveal_case(self):
+        self.is_revealed = True
+        
+    def toggle_mark(self):
+        match self.is_marked:
+            case 0:
+                self.is_marked = 1
+            case 1:
+                self.is_marked = 2
+            case 2:
+                self.is_marked = 0
